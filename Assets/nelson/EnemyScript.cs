73b08b861f81f65e22c4ed5cc76bcba0deb0424b
nMontieI <118293295+nMontieI@users.noreply.github.com>
2023-03-01 09:43:48 -0800

    Enemy1.0
    Enemy Script for chasing the player fully finished

--- Assets/nelson/EnemyScript.cs
+++ Assets/nelson/EnemyScript.cs
@@ -7,6 +7,7 @@ public class EnemyScript : MonoBehaviour
 {
     public static event Action<EnemyScript> OnEnemyKilled;
     [SerializeField] float health, maxHealth = 3f;
+    [SerializeField] float moveSpeed = 5f;
     Rigidbody2D rb;
     Transform target;
     Vector2 moveDirection;
@@ -21,9 +22,28 @@ public class EnemyScript : MonoBehaviour
     private void Start()
     {
         health = maxHealth;
+        target = GameObject.Find("Player").transform;
+
     }
 
 
+    private void Update()
+    {
+        if (target)
+        {
+            Vector3 direction = (target.position - transform.position).normalized;
+            float angle = Mathf.Atan2(direction.y,direction.x) * Mathf.Rad2Deg;
+            rb.rotation = angle;
+            moveDirection = direction;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (target)
+        {
+            rb.velocity = new Vector2(moveDirection.x, moveDirection.y) * moveSpeed;
+        }
+    }
 
 
     public void TakeDamage(float damageAmount)
