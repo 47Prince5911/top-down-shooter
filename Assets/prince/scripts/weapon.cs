ce5eab0e5b4516dbeec501ee70ee0ab2da4d88a3
47Prince5911 <118293615+47Prince5911@users.noreply.github.com>
2023-03-03 10:21:52 -0800

    1.5.1

---
+++ Assets/prince/scripts/weapon.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform firepoint;
+    public float fireForce = 20f;
+
+    public void Fire()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firepoint.position, firepoint.rotation);
+        bullet.GetComponent<Rigidbody2D>().AddForce(firepoint.up * fireForce, ForceMode2D.Impulse);
+    }
+}
